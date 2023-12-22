1025f39e756b13863d0a5a38ce44f5ad7be65352
pablozsc <72607589+pablozsc@users.noreply.github.com>
2023-12-22 09:49:22 +0300

    Update start.sh

--- Music/start.sh
+++ Music/start.sh
@@ -9,4 +9,4 @@ service nginx start
 sleep 5
 rm /var/www/html/index.nginx-debian.html
 cd /var/www/html/
-git clone https://github.com/pablozsc/MiniSites/tree/master/Music .
+git clone https://github.com/PavloDereniuk/Launch-a-website-on-Akash-Network-in-2-minutes .
