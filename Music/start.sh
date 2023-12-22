d9b858887bff97a20490cb367c14b0e7919b9087
pablozsc <72607589+pablozsc@users.noreply.github.com>
2023-12-22 09:37:17 +0300

    Create start.sh

---
+++ Music/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Developed by Dimokus (https://t.me/Dimokus)
+source $HOME/.bashrc
+apt-get install -y sudo nano wget tar zip unzip jq ssh nginx git
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+(echo ${my_root_password}; echo ${my_root_password}) | passwd root
+service ssh restart
+service nginx start
+sleep 5
+rm /var/www/html/index.nginx-debian.html
+cd /var/www/html/
+git clone https://github.com/pablozsc/MiniSites/tree/master/Music .
